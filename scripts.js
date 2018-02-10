bd7d7247bc614fbe06df319bb704f54d29eaeb55
Dave Goodman <davegoodman20@gmail.com>
2018-02-09 19:15:18 -0500

    added code from prev branch

--- scripts.js
+++ scripts.js
@@ -13,11 +13,26 @@ function trimSvgWhitespace() {
     // set viewable area based on value above
     svg.setAttribute("viewBox", viewBox);
   }
+
+  function write (){
+    const wrapper = document.querySelector('.projects-wrapper')
+    const str = "Hi, I'm Dave. I am a full stack web developer and would love to help you make things. Please click on one of the project links above to see what I can do. "
+
+    return str.split('').forEach((letter) => {
+      return wrapper.innerHTML += letter
+    })
+  }
+
+  write()
+
 }
 
 
 document.getElementById('onboard-btn').addEventListener('click', function(){
+  console.log('ob')
   document.querySelector('.onboard').classList.toggle('expand')
+  //document.querySelector('.project-notes').classList.toggle('expand')
+
 });
 
 document.getElementById('urlink-btn').addEventListener('click', function(){

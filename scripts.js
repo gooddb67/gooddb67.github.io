68046ea3e29ce4f57b3c7568d26f12634da154e6
Dave Goodman <davegoodman20@gmail.com>
2018-02-15 10:37:46 -0500

    updated js

--- scripts.js
+++ scripts.js
@@ -1,35 +1,173 @@
 window.onload = start()
 
 function start(){
-  const navBox = document.querySelector('.nav-box');
-  const btns = navBox.getElementsByClassName("project-btn");
-  const projectsWrapper = document.querySelector('.projects-wrapper')
-  const projects = projectsWrapper.getElementsByClassName("project")
+  setTimeout(function(){
+    type()
+  }, 700)
 
-  for (let i = 0; i < btns.length; i++){
-    btns[i].addEventListener("click", function(){
-      let currentBtn = document.getElementsByClassName("active-btn");
-      currentBtn[0].className = currentBtn[0].className.replace(" active-btn", "");
-      this.className += " active-btn"
-    });
+  setTimeout(function(){
+    type1()
+  }, 1500)
 
-  }
+
+  setTimeout(function(){
+    type2()
+  }, 4000)
+
+  setTimeout(function(){
+    type3()
+  }, 6500)
+
+  setTimeout(function(){
+    type4()
+  }, 9000)
+
+  setTimeout(function(){
+    type5()
+  }, 11500)
+
+  setTimeout(function(){
+    type6()
+  }, 13500)
+
+  setTimeout(function(){
+    type7()
+  }, 15000)
+
+}
+
+
+
+
+function type(){
+  var str = "Hi, I'm Dave!";
+  var spans = '<span>' + str.split('').join('</span><span>') + '</br></br></span>';
+  $(spans).hide().appendTo('.welcome-text').each(function (i) {
+      $(this).delay(45 * i).css({
+          display: 'inline',
+          opacity: 0
+      }).animate({
+          opacity: 1
+      }, 50);
+  });
+}
+
+function type1(){
+  var str = "I'm a full stack web developer based in Brooklyn.";
+
+
+  var spans = '<span>' + str.split('').join('</span><span>') + '</br></br></span>';
+  $(spans).hide().appendTo('.welcome-text').each(function (i) {
+      $(this).delay(45 * i).css({
+          display: 'inline',
+          opacity: 0
+      }).animate({
+          opacity: 1
+      }, 50);
+  });
+}
+
+function type2(){
+ var str2 = "I like to use the stuff in the orange box."
+ var spans = '<span>' + str2.split('').join('</span><span>') + '</br></br></span>';
+ $(spans).hide().appendTo('.welcome-text').each(function (i) {
+     $(this).delay(45 * i).css({
+         display: 'inline',
+         opacity: 0
+     }).animate({
+         opacity: 1
+     }, 50);
+ });
+}
+
+function type3(){
+ var str3 = "Click the buttons above to see what I've made."
+ var spans = '<span>' + str3.split('').join('</span><span>') + '</br></br></span>';
+ $(spans).hide().appendTo('.welcome-text').each(function (i) {
+     $(this).delay(45 * i).css({
+         display: 'inline',
+         opacity: 0
+     }).animate({
+         opacity: 1
+     }, 50);
+ });
+}
+
+function type4(){
+ var str4 = "There's a bunch of ways to get a hold of me."
+ var spans = '<span>' + str4.split('').join('</span><span>') + '</br></br></span>';
+ $(spans).hide().appendTo('.welcome-text').each(function (i) {
+     $(this).delay(45 * i).css({
+         display: 'inline',
+         opacity: 0
+     }).animate({
+         opacity: 1
+     }, 50);
+ });
+}
+
+function type5(){
+ var str5 = "Just click one of those icons below."
+ var spans = '<span>' + str5.split('').join('</span><span>') + '</br></br></span>';
+ $(spans).hide().appendTo('.welcome-text').each(function (i) {
+     $(this).delay(45 * i).css({
+         display: 'inline',
+         opacity: 0
+     }).animate({
+         opacity: 1
+     }, 50);
+ });
+}
+
+function type6(){
+ var str6 = "Let's talk soon!"
+ var spans = '<span>' + str6.split('').join('</span><span>') + '</br></br></span>';
+ $(spans).hide().appendTo('.welcome-text').each(function (i) {
+     $(this).delay(45 * i).css({
+         display: 'inline',
+         opacity: 0
+     }).animate({
+         opacity: 1
+     }, 50);
+ });
+}
+
+function type7(){
+ var str7 = "--Dave"
+ var spans = '<span>' + str7.split('').join('</span><span>') + '</br></br></span>';
+ $(spans).hide().appendTo('.welcome-text').each(function (i) {
+     $(this).delay(45 * i).css({
+         display: 'inline',
+         opacity: 0
+     }).animate({
+         opacity: 1
+     }, 100);
+ });
 }
 
 
 document.getElementById('onboard-btn').addEventListener('click', function(){
+  this.classList.add('active-btn')
+  document.querySelector('#urlink-btn').classList.remove('active-btn')
+  document.querySelector('#stagefast-btn').classList.remove('active-btn')
   document.querySelector('.urlink').classList.remove('expand')
   document.querySelector('.stagefast').classList.remove('expand')
   document.querySelector('.onboard').classList.toggle('expand')
 });
-
+//
 document.getElementById('urlink-btn').addEventListener('click', function(){
+  this.classList.add('active-btn')
+  document.querySelector('#stagefast-btn').classList.remove('active-btn')
+  document.querySelector('#onboard-btn').classList.remove('active-btn')
   document.querySelector('.onboard').classList.remove('expand')
   document.querySelector('.stagefast').classList.remove('expand')
   document.querySelector('.urlink').classList.toggle('expand')
 });
-
+//
 document.getElementById('stagefast-btn').addEventListener('click', function(){
+  this.classList.add('active-btn')
+  document.querySelector('#urlink-btn').classList.remove('active-btn')
+  document.querySelector('#onboard-btn').classList.remove('active-btn')
   document.querySelector('.urlink').classList.remove('expand')
   document.querySelector('.onboard').classList.remove('expand')
   document.querySelector('.stagefast').classList.toggle('expand')
